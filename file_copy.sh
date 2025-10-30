18c7d1bef8146b644dcbfd4df2e434093c5005e2
boysbeanxious <ccunyday@naver.com>
2025-10-30 14:53:31 +0900

    20251030

--- file_copy.sh
+++ file_copy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "file copy"
 declare -a ele_list
-ele_list=("TPU" "GaN" "Ni(OH)2" "SiO2" "BaTiO3" "Al2O3" "Co-Oxide" "Cu-Oxide" "NiSO4" "TiO2" "Li2CO3")
+ele_list=("TPU" "GaN" "Ni(OH)2" "White Carbon (SiO2)" "BaTiO3" "Al2O3" "Co-Oxide" "Cu-Oxide" "NiSO4" "TiO2" "Li2CO3")
                 
 for ele in "${ele_list[@]}"; do
 	echo $ele
 	echo "--------------------"
 	new_file_nm="TERGM_11ele_$ele.Rmd"
 	echo $new_file_nm
-	cp ./TERGM_11ele.Rmd $new_file_nm
+	cp ./TERGM_11ele.Rmd "$new_file_nm"
 
 	sed -i '' "s/"ELEMENT_NAME_HERE"/${ele}/g" "$new_file_nm"
 done
